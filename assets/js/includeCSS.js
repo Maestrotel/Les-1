e79caeb3d1d99ac33c280023d334db4732aedf8a
nevvord <nevvord@gmail.com>
2020-08-11 21:08:03 +0300

    fixes css includes

--- assets/js/includeCSS.js
+++ assets/js/includeCSS.js
@@ -1,7 +1,15 @@
-function addCss(name) {
+function addCss(path) {
   const link = document.createElement('link')
   link.rel = "stylesheet"
-  link.href = `/assets/styles/${name}.css`
+  link.href = `/assets/styles/${path}.css`
   const head = document.getElementsByTagName('head')
   head[0].appendChild(link)
 }
+
+// const addCss = (path) => {
+//   const link = document.createElement('link')
+//   link.rel = "stylesheet"
+//   link.href = `/assets/styles/${path}.css`
+//   const head = document.getElementsByTagName('head')
+//   head[0].appendChild(link)
+// }
